85a5a6b334d6e23f08592c82d9a8dbb05d5f170e
Sherif Abdelhamid <sherif@cos.io>
2017-08-29 15:43:12 -0400

    merge with develop

--- app/routes/application.js
+++ app/routes/application.js
@@ -10,4 +10,27 @@ import OSFAgnosticAuthRouteMixin from 'ember-osf/mixins/osf-agnostic-auth-route'
 /**
  * @class Application Route Handler
  */
-export default Ember.Route.extend(OSFAgnosticAuthRouteMixin);
+export default Ember.Route.extend(OSFAgnosticAuthRouteMixin, {
+    i18n: Ember.inject.service(),
+    afterModel: function() {
+        const availableLocales = this.get('i18n.locales').toArray();
+        let locale;
+
+        if (navigator.languages && navigator.languages.length) {
+            // Works in Chrome and Firefox (editable in settings)
+            for (let lang of navigator.languages) {
+                if (availableLocales.includes(lang)) {
+                    locale = lang;
+                    break;
+                }
+            }
+        } else if (navigator.language && availableLocales.includes(navigator.language)) {
+            // Backup for Safari (uses system settings)
+            locale = navigator.language;
+        }
+
+        if (locale) {
+            this.set('i18n.locale', locale);
+        }
+    }
+});
