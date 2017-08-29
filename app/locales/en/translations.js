cad15efbb4c5288eec8f81f18fd2d07b31328ca6
Sherif Abdelhamid <sherif@cos.io>
2017-08-29 08:56:31 -0400

    [MOD-65][feature] Move plain text to the translation file (#9)

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -17,4 +17,28 @@ export default {
         pre_moderation: `pre-moderation`,
         post_moderation: `post-moderation`,
     },
+    index: {
+        feature: {
+            title: `Moderate your collection`,
+            description: `At last, the ability to manage what scholarly works are displayed with your branding. 
+            No more working on off-topic papers confusing loyal researchers.`,
+            list_1: `See all submissions in one place.`,
+            list_2: `Provide feedback to authors.`,
+            list_3: `Manage collections settings.`
+        },
+        workflow: {
+            title: `Choose your workflow`,
+            list_1: `Have greater control over what material is publicly available by choosing pre-moderation.`,
+            list_2: `Keep the time delay between submission and public access minimal by choosing post-moderation.`,
+            figure: {
+                pre_moderation: `Pre-moderation`,
+                post_moderation: `Post-moderation`
+            }
+        },
+        contact: {
+            title: `Want to start a moderated service?`,
+            paragraph: `Create your own branded preprints servers backed by the OSF. Check out the open source code and the requirements and road map. Input welcome!`,
+            contact_us: `Contact us`
+        }
+    }
 };
