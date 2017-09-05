67e692e8778939e5d19b06bd94abc0f04f8362d0
Sherif Abdelhamid <sherif@cos.io>
2017-09-05 13:41:04 -0400

    Update

--- app/components/preprint-record/component.js
+++ app/components/preprint-record/component.js
@@ -14,14 +14,14 @@ import Ember from 'ember';
  **/
 export default Ember.Component.extend({
     theme: Ember.inject.service(),
-    preprintRecords: Ember.computed('data', 'type', function () {
+    iconClass: {
+        accepted: "fa fa-check-circle-o fa-lg",
+        pending: "fa fa-hourglass-o fa-lg",
+        rejected: "fa fa-times-circle-o fa-lg"
+    },
+    preprintRecords: Ember.computed('i18n.locale', 'data', 'type', function () {
         const type = this.get('type');
         return type == 'accepted' ? this.get('data.acceptedPreprints.records') : type == 'pending' ?
                 this.get('data.pendingPreprints.records') : this.get('data.rejectedPreprints.records')
-    }),
-    iconClass: Ember.computed('type', function () {
-        const type = this.get('type');
-        return type == 'accepted' ? "fa fa-check-circle-o fa-lg" : type == 'pending' ?
-                "fa fa-hourglass-o fa-lg" : "fa fa-times-circle-o fa-lg"
     })
 });
