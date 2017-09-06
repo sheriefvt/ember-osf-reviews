262abdf4d121efc6b00cbc4fc1bf06e1f654716e
Chris Seto <chriskseto@gmail.com>
2017-09-06 09:00:25 -0400

    Merge conflict fixes and reformatting

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,6 +4,14 @@ export default Ember.Controller.extend({
     session: Ember.inject.service(),
     currentUser: Ember.inject.service(),
 
+    showSetup: Ember.computed.bool('providersToSetup'),
+
+    providersToSetup: Ember.computed('model.[]', function() {
+        return this.get('model').filter(provider =>
+            !provider.get('reviewsWorkflow') && provider.get('permissions').includes('set_up_moderation')
+        );
+    }),
+
     showDashboard: Ember.computed('model.[]', function() {
         const providers = this.get('model');
         return providers.any((p) => p.get('reviewsWorkflow'));
@@ -12,6 +20,9 @@ export default Ember.Controller.extend({
     actions: {
         transitionToDetail(reviewable) {
             this.transitionToRoute('provider.preprint_detail', [reviewable.get('provider'), reviewable]);
-        }
-    },
+        },
+        setupProvider(provider) {
+            this.transitionToRoute('preprints.provider.setup', provider);
+        },
+    }
 });
