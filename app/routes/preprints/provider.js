eb4aab623d301d78bd3ea9294078765e4b6e5636
Lauren Barker <lauren@cos.io>
2017-09-19 14:26:28 -0400

    Prevent inifinate reload (#25)

--- app/routes/preprints/provider.js
+++ app/routes/preprints/provider.js
@@ -22,7 +22,9 @@ export default Ember.Route.extend({
         didTransition() {
             // TODO: make this less hacky
             // force the provider to reload on page transitions so statusCounts update
-            this.refresh();
+            if (!this.controller.get('model.reviewableStatusCounts')) {
+                this.refresh();
+            }
         }
     }
 });
