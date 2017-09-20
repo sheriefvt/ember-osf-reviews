7913983f3792f366506160c803244d5ff9d16aec
Sherif Abdelhamid <sherif@cos.io>
2017-09-20 12:34:46 -0400

    scroll reset on route transition

--- app/router.js
+++ app/router.js
@@ -3,7 +3,19 @@ import config from './config/environment';
 
 const Router = Ember.Router.extend({
     location: config.locationType,
-    rootURL: config.rootURL
+    rootURL: config.rootURL,
+    willTransition(oldInfos, newInfos) {
+        const disableList = ['settings', 'moderation'];
+        const prevRoute = oldInfos.length ? oldInfos[oldInfos.length - 1].name.split('.') : null;
+        const prevName = prevRoute != null ? prevRoute[prevRoute.length - 1] : null;
+        const currentRoute = newInfos.length ? newInfos[newInfos.length - 1].name.split('.') : null;
+        const currentName = currentRoute != null ? currentRoute[currentRoute.length - 1] : null;
+        const disableResetScroll = disableList.includes(prevName) && disableList.includes(currentName);
+        if (!disableResetScroll) {
+            this._super(...arguments);
+            window.scrollTo(0, 0);
+        }
+    }
 });
 
 Router.map(function() {
