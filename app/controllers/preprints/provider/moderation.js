bd891ebba96f0bdea5c191ce86cea3c28033e631
Sherif Abdelhamid <sherif@cos.io>
2017-09-28 16:53:53 -0400

    Use Google Analytics

--- app/controllers/preprints/provider/moderation.js
+++ app/controllers/preprints/provider/moderation.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import Analytics from 'ember-osf/mixins/analytics';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(Analytics, {
     queryParams: ['page', 'sort', 'status'],
     page: 1,
     status: 'pending',
@@ -13,6 +14,12 @@ export default Ember.Controller.extend({
         },
         pageChanged(page) {
             this.set('page', page);
+            Ember.get(this, 'metrics')
+                .trackEvent({
+                    category: 'button',
+                    action: 'click',
+                    label: `Request page ` + this.get('page') + ` of ` + this.get('status') + ` list`
+                });
         },
         sortChanged(sort) {
             this.set('sort', sort);
--- tests/unit/routes/preprints/provider/moderation-test.js
+++ tests/unit/routes/preprints/provider/moderation-test.js
@@ -3,7 +3,8 @@ import { moduleFor, test } from 'ember-qunit';
 moduleFor('route:preprints/provider/moderation', 'Unit | Route | preprints/provider/moderation', {
   // Specify the other units that are required for this test.
   needs: [
-    'service:theme',
+      'service:theme',
+      'service:metrics'
   ]
 });
 
