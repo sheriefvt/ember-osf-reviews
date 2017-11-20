6caf65c71c00a00263cd826d96fa8ac012bc24a8
Sherif Abdelhamid <sherif@cos.io>
2017-11-19 21:55:24 -0500

    Remove unnecessary import

--- tests/integration/components/preprint-status-banner/component-test.js
+++ tests/integration/components/preprint-status-banner/component-test.js
@@ -1,4 +1,3 @@
-import EmberObject from '@ember/object';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import wait from 'ember-test-helpers/wait';
